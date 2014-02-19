b720c2ed801b61cb340999c689cd1f2d044d7157
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-02-18 22:25:16 -0500

    Remove seqinr call

--- R/instaindex.R
+++ R/instaindex.R
@@ -1,5 +1,4 @@
 instaindex<-function(seq){
-  require(seqinr)
   seq<-s2c(seq)
   AA<-matrix(nrow=1,ncol=20)
   colnames(AA)<-c("I","L","W","F","V","M","Y","A","P","T","S","C","G","N","D","Q","E","H","K","R")
