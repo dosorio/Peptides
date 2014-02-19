aaa439b4cc2a18b5ab3bea258830be446ced251c
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-02-19 15:28:31 -0500

    New function Molecular weight

---
+++ R/MW.R
@@ -0,0 +1,10 @@
+MW<-function(seq){
+  require(seqinr) ; seq<-s2c(seq)
+  W<-aaindex$FASG760101$I ; names(W)<-a(names(W)) ; W1<-table(seq)
+  weight<-0 ; for (AA in 1:length(W1)){
+    if(names(W1)[AA]==names(W)[which(names(W)==names(W1)[AA])]){
+      weight<-weight+(W1[AA]*W[which(names(W)==names(W1)[AA])])
+    }
+  }
+  return(as.vector(weight))
+}
