2e01db6484581d5d5742dbc84727e13dbf0418b0
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-03-03 11:24:38 -0500

    Update function AAcomp
    Update the man page, change the example and modify the number of
    decimal on the output

--- R/AAcomp.R
+++ R/AAcomp.R
@@ -9,14 +9,14 @@ AAcomp<-function(seq){
       }
     } 
   }
-  CM[1,1]<-sum(AA[c(1,3,7,19,20)]) ; CM[1,2]<-round(CM[1,1]/length(s2c(seq)),5)*100
-  CM[2,1]<-sum(AA[c(1,2,3,4,7,14,16,19,20,22)]); CM[2,2]<-round(CM[2,1]/length(s2c(seq)),5)*100
-  CM[3,1]<-sum(AA[c(1,9,12,22)]); CM[3,2]<-round(CM[3,1]/length(s2c(seq)),5)*100
-  CM[4,1]<-sum(AA[c(6,8,23,25)]); CM[4,2]<-round(CM[4,1]/length(s2c(seq)),5)*100
-  CM[5,1]<-sum(AA[c(1,3,6,7,9,12,13,16,22,23,25)]); CM[5,2]<-round(CM[5,1]/length(s2c(seq)),5)*100
-  CM[6,1]<-sum(AA[c(4,5,8,11,14,17,18,19,20,26)]); CM[6,2]<-round(CM[6,1]/length(s2c(seq)),5)*100
-  CM[7,1]<-sum(AA[c(2,4,5,8,11,18,26)]); CM[7,2]<-round(CM[7,1]/length(s2c(seq)),5)*100
-  CM[8,1]<-sum(AA[c(8,11,18)]); CM[8,2]<-round(CM[8,1]/length(s2c(seq)),5)*100
-  CM[9,1]<-sum(AA[c(2,4,5,26)]); CM[9,2]<-round(CM[9,1]/length(s2c(seq)),5)*100
+  CM[1,1]<-sum(AA[c(1,3,7,19,20)]) ; CM[1,2]<-round(CM[1,1]/length(s2c(seq)),4)*100
+  CM[2,1]<-sum(AA[c(1,2,3,4,7,14,16,19,20,22)]); CM[2,2]<-round(CM[2,1]/length(s2c(seq)),4)*100
+  CM[3,1]<-sum(AA[c(1,9,12,22)]); CM[3,2]<-round(CM[3,1]/length(s2c(seq)),4)*100
+  CM[4,1]<-sum(AA[c(6,8,23,25)]); CM[4,2]<-round(CM[4,1]/length(s2c(seq)),4)*100
+  CM[5,1]<-sum(AA[c(1,3,6,7,9,12,13,16,22,23,25)]); CM[5,2]<-round(CM[5,1]/length(s2c(seq)),4)*100
+  CM[6,1]<-sum(AA[c(4,5,8,11,14,17,18,19,20,26)]); CM[6,2]<-round(CM[6,1]/length(s2c(seq)),4)*100
+  CM[7,1]<-sum(AA[c(2,4,5,8,11,18,26)]); CM[7,2]<-round(CM[7,1]/length(s2c(seq)),4)*100
+  CM[8,1]<-sum(AA[c(8,11,18)]); CM[8,2]<-round(CM[8,1]/length(s2c(seq)),4)*100
+  CM[9,1]<-sum(AA[c(2,4,5,26)]); CM[9,2]<-round(CM[9,1]/length(s2c(seq)),4)*100
   return(CM)
 }
