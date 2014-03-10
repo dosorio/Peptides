d6b2e13f405101be047002468727c59379c69ac0
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-03-10 14:04:22 -0500

    Update function Membrane Position

--- R/MembranePos.R
+++ R/MembranePos.R
@@ -1,12 +1,12 @@
 MembranePos<-function(seq,structure){
-  if(hmoment(seq,structure)<((-0.4*h(seq))+0.6) & (h(seq)>0.5)){
+  if(hmoment(seq,structure)<((-42.1*h(seq))+57.9) & (h(seq)>0.5)){
     print("Transmembrane")
   } else{
-    if(hmoment(seq,structure)<((-0.4*h(seq))+0.6) & (h(seq)<0.5)){
+    if(hmoment(seq,structure)<((-42.1*h(seq))+57.9) & (h(seq)<0.5)){
       print("Globular")
     }
     else{
-      if(hmoment(seq,structure)>((-0.4*h(seq))+0.6)){
+      if(hmoment(seq,structure)>((-42.1*h(seq))+57.9)){
         print("Surface")
       }
     }
