b87d71f6af0e8c8b4c30c4ed6ebf61d85a7515d2
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-03-25 17:33:21 -0500

    New version 0.4

--- R/MW.R
+++ R/MW.R
@@ -1,9 +1,13 @@
 mw<-function(seq){
   seq<-s2c(seq)
-  W<-aaindex$FASG760101$I ; names(W)<-a(names(W)) ; W1<-table(seq)
-  weight<-0 ; for (AA in 1:length(W1)){
-    if(names(W1)[AA]==names(W)[which(names(W)==names(W1)[AA])]){
-      weight<-weight+(W1[AA]*W[which(names(W)==names(W1)[AA])])
+  W<-aaindex$FASG760101$I ; names(W)<-a(names(W)) ; W1<-table(seq) ; weight<-0 
+  for (AA in 1:length(W1)){
+    if(names(W1)[AA]=="X"){
+      weight<-weight+0
+    }else{
+      if(names(W1)[AA]==names(W)[which(names(W)==names(W1)[AA])]){
+        weight<-weight+(W1[AA]*W[which(names(W)==names(W1)[AA])])
+      }
     }
   }
   return(as.vector(weight))
