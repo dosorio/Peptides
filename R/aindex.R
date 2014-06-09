61ed350410f6fa4244035d51c9f78eb3dffa7690
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-06-09 17:56:04 -0500

    Vectorized aindex

--- R/aindex.R
+++ R/aindex.R
@@ -1,20 +1,4 @@
-aindex<-function(seq){
-  Val<-Ala<-Leu<-Ile<-0
-  AA<-table(s2c(seq))
-  for(i in 1:length(AA)){
-    if(names(AA)[i]=="V"){
-      Val<-100*(as.numeric(AA)[i]/length(s2c(seq)))
-    } 
-    else if(names(AA)[i]=="A"){
-      Ala<-100*(as.numeric(AA)[i]/length(s2c(seq)))
-    }
-    else if(names(AA)[i]=="L"){
-      Leu<-100*(as.numeric(AA)[i]/length(s2c(seq)))
-    }
-    else if(names(AA)[i]=="I"){
-      Ile<-100*(as.numeric(AA)[i]/length(s2c(seq)))
-    }
-  }
-  ai<-round((Ala+(2.9*Val)+(3.9*(Leu+Ile))),2)
-  return(ai)
+aindex1<-function(seq){
+  p<-100*(table(s2c(seq))/sum(table(s2c(seq))))
+  round(sum(c(p["A"],(2.9*p["V"]),3.9*p[c("L","I")]),na.rm=T),2)
 }
