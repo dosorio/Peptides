e4e2f88e029eddab581c5adb76e7dd0246f8f7b9
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-07-05 11:27:46 -0500

    Hmoment function

--- R/hmoment.R
+++ R/hmoment.R
@@ -1,19 +1,20 @@
 hmoment1<-function(seq,angle){
+ data(H, envir = environment())
   cos<-sin<-uH<-NULL
   moment<-function(seq,angle){
     for (i in 1: nchar(seq)){
-      cos[i]<-(Eisenberg[s2c(seq)[i]]*(cos((angle*(pi/180))*i)))
-      sin[i]<-(Eisenberg[s2c(seq)[i]]*(sin((angle*(pi/180))*i)))}
+      cos[i]<-(as.array(H[[12]])[s2c(seq)[i]]*(cos((angle*(pi/180))*i)))
+      sin[i]<-(as.array(H[[12]])[s2c(seq)[i]]*(sin((angle*(pi/180))*i)))}
     round(sqrt(sum(cos,na.rm=TRUE)^2+sum(sin,na.rm=TRUE)^2)/nchar(seq),2)
   }
-  if (nchar(seq)<10){
+  if (nchar(seq)<=10){
     moment(seq,angle) 
   }else{
     if (nchar(seq)>10){
       for(j in 1: (nchar(seq)-9)){
         uH[j]<-(moment(paste(s2c(seq)[j:(j+9)],collapse=""),angle))
       }
-      max(uH)
+      return(max(uH))
     }
   }
 }
