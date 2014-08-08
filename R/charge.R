c0bc85178abd88347a5ad47a52d2c47e04e3b210
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-08-08 17:13:08 -0500

    New charge function

--- R/charge.R
+++ R/charge.R
@@ -5,10 +5,28 @@
 # The algorithm is the same as the one which is implemented at the following url: 
 # http://www.expasy.org/tools/pi_tool-doc.html
 
-charge <- function(seq, pH, pKa){
-  AA<-table(s2c(seq))
-  pKa<-pmatch(pKa,names(pK))
-  pKa<-as.vector(pK[,2])
-  names(pKa)<-rownames(pK)
-  pKa[AA]
+charge <- function(seq,pH,pKa){
+  computeCharge <- function(pH, compoAA, pK, nTermResidue, cTermResidue){
+    cter <- 10^(-SEQINR.UTIL$pk[cTermResidue,1]) / 
+      (10^(-SEQINR.UTIL$pk[cTermResidue,1]) + 10^(-pH))
+    nter <- 10^(-pH) / (10^(-SEQINR.UTIL$pk[nTermResidue,2]) + 10^(-pH))
+    carg <- as.vector(compoAA['R'] * 10^(-pH) / (10^(-pK['R']) + 10^(-pH)))
+    chis <- as.vector(compoAA['H'] * 10^(-pH) / (10^(-pK['H']) + 10^(-pH)))
+    clys <- as.vector(compoAA['K'] * 10^(-pH) / (10^(-pK['K']) + 10^(-pH)))
+    casp <- as.vector(compoAA['D'] * 10^(-pK['D']) /(10^(-pK['D']) + 10^(-pH)))
+    cglu <- as.vector(compoAA['E'] * 10^(-pK['E']) / (10^(-pK['E']) + 10^(-pH)))
+    ccys <- as.vector(compoAA['C'] * 10^(-pK['C']) / (10^(-pK['C']) + 10^(-pH)))
+    ctyr <- as.vector(compoAA['Y'] * 10^(-pK['Y']) / (10^(-pK['Y']) + 10^(-pH)))
+    charge <- carg + clys + chis + nter - (casp + cglu + ctyr + ccys + cter)
+    return(charge)
+  }
+  prot <- s2c(seq)
+  compoAA <- table(factor(prot, levels = LETTERS))
+  nTermR <- which(LETTERS == prot[1])
+  cTermR <- which(LETTERS == prot[length(seq)])
+  pHseq <- pH
+  M<-pmatch(pKa,names(pK))
+  pKm<-pK[,M]
+  names(pKm) <- rownames(pK)
+  round(computeCharge(pHseq, compoAA, pKm, nTermR, cTermR),1)
 }
