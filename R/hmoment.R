0c33db7310b58a463fa7eac99150bfe3baf409ce
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-08-11 14:50:10 -0500

    Update function

--- R/hmoment.R
+++ R/hmoment.R
@@ -1,3 +1,9 @@
+# HMOMENT
+# This function compute the hmoment based on Eisenberg, D., Weiss, R. M., & Terwilliger, T. C. (1984). 
+# The hydrophobic moment detects periodicity in protein hydrophobicity. 
+# Proceedings of the National Academy of Sciences of the United States of America, 81(1), 140–4.
+# The hydrophobicity scale is the reported in: 
+
 hmoment<-function(seq,angle){
  data(H, envir = environment())
   cos<-sin<-uH<-NULL
