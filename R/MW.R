38215d2fa045023627ff6736f7c4f94ad6e82c74
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-08-15 10:41:36 -0500

    update mw

--- R/MW.R
+++ R/MW.R
@@ -1,3 +1,9 @@
+# MW
+# This function calculates the molecular weight of a protein sequence using the formula and scale available on ExPASy MWtool
+# Gasteiger, E., Hoogland, C., Gattiker, A., Wilkins, M. R., Appel, R. D., & Bairoch, A. (2005). 
+# Protein identification and analysis tools on the ExPASy server. 
+# In The proteomics protocols handbook (pp. 571-607). Humana Press.
+
 mw<-function(seq){
   weight<-c(A=71.0788, R=156.1875, N=114.1038, D=115.0886, C=103.1388, E=129.1155, Q=128.1307, 
             G=57.0519, H=137.1411, I=113.1594, L=113.1594, K=128.1741, M=131.1926, F=147.1766, 
