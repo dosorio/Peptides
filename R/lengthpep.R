a981be99374f171afb636a2d3955fe6a3f575a9d
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-08-23 22:17:51 -0500

    update lengthpep function + comments

--- R/lengthpep.R
+++ R/lengthpep.R
@@ -2,10 +2,6 @@
 # This function counts the amino acids of a protein sequence
 
 lengthpep<-function(seq){
-  # Check for not amino acid letters
-  if (grepl("J",seq,ignore.case = TRUE)){
-    return("The sequence you entered contains illegal characters: The letter J does not code for an amino acid. Please go back and verify your sequence.")
-  }
-  # Count the amino acids
+    # Count the amino acids
   nchar(seq)
 }
