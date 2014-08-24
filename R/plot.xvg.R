21320ecea3f9a1633a875097c5b73b95008f5b71
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-08-23 19:54:02 -0500

    update plot.xvg function + comments

--- R/plot.xvg.R
+++ R/plot.xvg.R
@@ -1,8 +1,15 @@
+# PLOT.XVG
+# This function plot the XVG output file from GROMACS molecular dynamics package
+
 plot.xvg<-function(x, ...){
+  # Load data
   data<-read.xvg(x)
+  # Read file
   X<-readLines(x)
+  # Set Labels
   P1<-grep(pattern="title",x=X[1:length(X)])
   P2<-grep(pattern="s0",x=X[1:length(X)])
+  # Plot
   plot(data[,1],data[,2],type="l",
        main=(scan(x,skip=min(P1)-1,nmax=min(P1)+2,what="",quiet=TRUE)[3]),
        xlab=(scan(x,skip=min(P1)-1,nmax=min(P1)+2,what="",quiet=TRUE)[7]),
