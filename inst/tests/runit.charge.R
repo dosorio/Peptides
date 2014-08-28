285e1acecfaca668805e853d262f9e51f77917cb
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-08-27 22:04:04 -0500

    update runit charge

--- inst/tests/runit.charge.R
+++ inst/tests/runit.charge.R
@@ -2,14 +2,14 @@ test.charge <- function(){
   # COMPARED TO EMBOSS PEPSTATS
   # SEQUENCE: FLPVLAGLTPSIVPKLVCLLTKKC
   # Charge   = 3.0
-  checkEquals(charge("FLPVLAGLTPSIVPKLVCLLTKKC",7,pKscale = "EMBOSS"),3)
+  checkEquals(round(charge("FLPVLAGLTPSIVPKLVCLLTKKC",7,pKscale = "EMBOSS")),3)
   
   # COMPARED TO YADAMP
   # http://yadamp.unisa.it/showItem.aspx?yadampid=845&x=0,7055475
   # SEQUENCE: FLPVLAGLTPSIVPKLVCLLTKKC
-  checkEquals(charge("FLPVLAGLTPSIVPKLVCLLTKKC",5,pKscale = "Bjellqvist"),round(3,0))
-  checkEquals(charge("FLPVLAGLTPSIVPKLVCLLTKKC",7,pKscale = "Bjellqvist"),round(2.91,0))
-  checkEquals(charge("FLPVLAGLTPSIVPKLVCLLTKKC",9,pKscale = "Bjellqvist"),round(1.09,0))
+  checkEquals(round(charge("FLPVLAGLTPSIVPKLVCLLTKKC",5,pKscale = "Lehn"),1),3)
+  checkEquals(round(charge("FLPVLAGLTPSIVPKLVCLLTKKC",7,pKscale = "Lehn"),1),2.9)
+  checkEquals(round(charge("FLPVLAGLTPSIVPKLVCLLTKKC",9,pKscale = "Lehn"),1),1.0)
   
   # CHECK OUTPUT CLASS
   checkTrue(is.numeric(charge("FLPVLAGLTPSIVPKLVCLLTKKC",7,pKscale = "Bjellqvist")))
