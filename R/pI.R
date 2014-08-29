64be6c10ead72d1e1db5954fe62c5f4fa6b56061
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-08-28 20:57:22 -0500

    new pI function

--- R/pI.R
+++ R/pI.R
@@ -1,8 +1,19 @@
-# pI
-# This function compute the pI of a aminoacid sequence
-# This function is the same writen by Charif, D. and Lobry, J.R. (2007) on "seqinr 3.0-7" R package
-# Charif, D., & Lobry, J. R. (2007). SeqinR 1.0-2: a contributed package to the R project for 
-# statistical computing devoted to biological sequences retrieval and analysis.
-# In Structural approaches to sequence evolution (pp. 207-232). Springer Berlin Heidelberg.
+# PI
+# This function computes the theoretical pI of a protein sequence using one of the 9 pKa scales availables
+# Bjellqvist, EMBOSS, Murray, Sillero, Solomon, Stryer, Lehninger, Dawson or Rodwell
+# The algorithm is the same implemented on Charif, D. and Lobry, J.R. (2007) computepI function of seqinR 3.0.7 package
 
-pI<-function(seq)round(computePI(s2c(seq)),2)
+pI<-function (seq,pKscale) 
+{
+  # Function to value minimize
+  I <- function(p1, p2, p3) {
+    charge(pH = p1, seq = p2, pKscale = p3)^2
+  }
+  # Non-Linear Minimization
+  nlmres <- nlm(I, 7, p2 = seq, p3 = pKscale)$minimum
+  while (!identical(all.equal(nlmres$minimum, 0), TRUE)) {
+    nlmres <- nlm(I, runif(1, 0, 14), p2 = seq, p3 = pKscale)
+  }
+  # Return the theoretical isoelectric point rounded to 2 decimals
+  return(round(nlmres$estimate,3))
+}
