aa414f84dd2baaa0f784001cacbd0150fb3c01f9
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-09-30 07:51:11 -0500

    update hydrophobicity

--- R/hydrophobicity.R
+++ R/hydrophobicity.R
@@ -5,17 +5,17 @@
 # A simple method for displaying the hydropathic character of a protein. 
 # Journal of Molecular Biology, 157(1), 105–32.
 
-hydrophobicity<-function(seq,method){
+hydrophobicity<-function(seq,scale){
   # Setting the hydrophobicity scale
   M<-c("KyteDoolittle","AbrahamLeo",   "BullBreese",   "Guy",          "Miyazawa",     "Roseman",      "Wolfenden",   
        "Wilson",       "Cowan3.4",     "Aboderin",     "Sweet",        "Eisenberg",    "HoppWoods",    "Manavalan",   
        "BlackMould",   "Fauchere",     "Janin",        "Rao",          "Tanford",      "Welling",      "Parker",      
        "Cowan7.5",     "Chothia",      "Rose")
-  method<-pmatch(method,M)
+  method<-pmatch(scale,M)
   # Loading hydrophobicity scales
   data(H, envir = environment())
   H<-H
   # Sum the hydrophobicity of each amino acid and divide them between the sequence length
   # Return the GRAVY value rounded to 2 decimals
-  round(sum(as.array(H[[method]])[s2c(toupper(seq))],na.rm = TRUE)/nchar(seq),2)
+  round(sum(as.array(H[[scale]])[s2c(toupper(seq))],na.rm = TRUE)/nchar(seq),2)
 }
