9b605d8966acf29b40be39b8888b66f4de499603
Daniel Camilo Osorio <danielcamiloosorio@gmail.com>
2014-11-11 18:08:24 -0500

    update

--- R/membpos.R
+++ R/membpos.R
@@ -3,32 +3,24 @@
 # Eisenberg, D. (1984). Three-dimensional structure of membrane and surface proteins. 
 # Annual Review of Biochemistry, 53, 595–623. doi:10.1146/annurev.bi.53.070184.003115
 
-membpos<-function(seq,angle,window=11){ 
+membpos<-function(seq,angle=100){ 
   # Setting input length
   aa<-strsplit(toupper(seq),"")[[1]]
-  window<-min(length(aa),window)
-  pep<-embed(aa,window)
-  # Compute the hmoment and hydrophobicity for each amino acid window
-  data<-character(dim(pep)[[1]])
-  data$Pep<-as.vector(Pep)
-  data$H<-as.vector(sapply(Pep,function(x)hydrophobicity(x,"Eisenberg")))
-  data$uH<-round(as.vector(sapply(Pep,function(x)hmoment(x,angle))),2)
-  data$m<-((-0.421*data$H)+0.579)
-  
-  # Assigns a class depending on the hydrophobicity and hmoment
-  for (i in 1: length(Pep)){
-    if(data$uH[i]<=data$m[i] & data$H[i]>=0.5){
-      data$MembPos[i]<-"Transmembrane"
-    }else{
-      if (data$uH[i]<=data$m[i] & data$H[i]<=0.5){
-        data$MembPos[i]<-"Globular"
-      }
-      else{
-        if(data$uH[i]>=data$m[i]){
-          data$MembPos[i]<-"Surface"
-        }
-      }
-    }
+  window<-min(length(aa),10)
+  pep<-character(nchar(seq)-window)
+  for (i in 1: (nchar(seq)-window)){
+    pep[i]<-paste(aa[i:(i+window)],collapse = "")
   }
-  return(as.data.frame(data[-4]))
+  # Compute the hmoment and hydrophobicity for each amino acid window
+  data<-as.data.frame(matrix(nrow = length(pep),ncol = 5))
+  data[,1]<-pep
+  data[,2]<-round(as.vector(sapply(pep,function(x)hydrophobicity(x,"Eisenberg"))),3)
+  data[,3]<-round(as.vector(sapply(pep,function(x)hmoment(x,angle,window = 10))),3)
+  data[,4]<-(data[,2]*-0.421)+0.579
+  colnames(data)<-c("Pep","H","uH","m","MembPos")
+  # Assigns a class depending on the hydrophobicity and hmoment
+  data[which(data$uH<=data$m & data$H>=0.5),5]<-"Transmembrane"
+  data[which(data$uH<=data$m & data$H<=0.5),5]<-"Globular"
+  data[which(data$uH>=data$m),5]<-"Surface"
+  return(data[-4])
 }
