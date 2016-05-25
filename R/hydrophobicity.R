0592ec9a2af62b495036b8344a07974357981d9c
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2016-05-25 09:09:46 +0300

    rewrote hydrophobicity for speed, using stringi

--- R/hydrophobicity.R
+++ R/hydrophobicity.R
@@ -12,16 +12,19 @@ hydrophobicity <- function(seq, scale="KyteDoolittle"){
   data(list = "H", package = "Peptides", envir = e)
   # Setting the hydrophobicity scale
   scale <- match.arg(scale, names(e$H))
+  hydrophobicity_scores <- e$H[[scale]]
+  # For each amino acid, its score is the hydro. score times the number of 
+  # occurences
+  amino_acid_scores <- vapply(
+    names(hydrophobicity_scores),
+    function(aa)
+    {
+      hydrophobicity_scores[aa] * stri_count_fixed(seq, aa)
+    },
+    numeric(length(seq))
+  )
   # Sum the hydrophobicity of each amino acid and divide them between the sequence length
   # Return the GRAVY value
-  amino_acid_list <- .to_upper_chars(seq)
-  hydrophobicity_scores <- e$H[[scale]]
-  vapply(
-    amino_acid_list,
-    function(amino_acids)
-    {
-      sum(hydrophobicity_scores[amino_acids]) / length(amino_acids)
-    },
-    numeric(1)
-  )
+  sum_fn <- if(is.matrix(amino_acid_scores)) rowSums else sum
+  sum_fn(amino_acid_scores) / nchar(seq)
 }
