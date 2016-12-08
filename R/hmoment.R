75f27a66c6f609857f816f573d00d26a56f9ad39
Daniel Osorio <dcosorioh@unal.edu.co>
2016-12-08 18:32:24 -0500

    Update Peptides

--- R/hmoment.R
+++ R/hmoment.R
@@ -4,24 +4,26 @@
 # Proceedings of the National Academy of Sciences of the United States of America, 81(1), 140-4.
 # This function was written by an anonymous reviewer of the RJournal
 
-hmoment<-function(seq,angle=100,window=11){
+hmoment <- function(seq, angle = 100, window = 11) {
   # Loading hydrophobicity scale
-  seq<-gsub("[\r\n ]","",seq)
-  data(H,envir = environment())
-  H<-H
-  h<-H[["Eisenberg"]]
+  seq <- gsub("[[:space:]]", "", seq)
+  data(H, envir = environment())
+  H <- H
+  h <- H[["Eisenberg"]]
   # Splitting the sequence in amino acids
-  aa<-strsplit(toupper(seq),"")[[1]]
-  window<-min(length(aa),window)
+  aa <- lapply(seq, function(seq){unlist(strsplit(seq,""))})
   # Setting the sequence length
-  pep<-embed(aa,window)
+  pep <- lapply(aa, function(aa){embed(aa,min(c(length(aa),window)))})
   # Applying the hmoment function to each amino acids window
-  angle<- angle*(pi/180)*1:window
-  vcos<-h[t(pep)]*cos(angle)
-  vsin<-h[t(pep)]*sin(angle)
-  dim(vcos)<-dim(vsin)<-dim(t(pep))
-  vcos<-colSums(vcos,na.rm = TRUE)
-  vsin<-colSums(vsin,na.rm = TRUE)
-  # Return the max value
-  max(sqrt(vsin*vsin + vcos*vcos)/window)
+  hmoment <- lapply(pep, function(pep){
+    angle <- angle * (pi / 180) * 1:min(c(length(pep),window))
+    vcos <- h[t(pep)] * cos(angle)
+    vsin <- h[t(pep)] * sin(angle)
+    dim(vcos) <- dim(vsin) <- dim(t(pep))
+    vcos <- colSums(vcos, na.rm = TRUE)
+    vsin <- colSums(vsin, na.rm = TRUE)
+    # Return the max value
+    max(sqrt(vsin * vsin + vcos * vcos) / min(c(length(pep),window)))
+  })
+  return(unlist(hmoment))
 }
