cb3505c75bd82643129073e5b85acc2d645cf1ba
Daniel Osorio <dcosorioh@unal.edu.co>
2017-02-20 18:08:48 -0500

    Update Peptides

--- R/crosscovariance.R
+++ R/crosscovariance.R
@@ -1,11 +1,13 @@
 #' @export crossCovariance
 #' @title Compute the cross-covariance index of a protein sequence
 #' @description This function computes the Cruciani et al (2004) cross-covariance index. The lagged \code{crossCovariance} index is calculated for a lag 'd' using two descriptors 'f1' and 'f2' (centred) over a sequence of length 'L'.
+#'
 #' @param sequence An amino-acids sequence
 #' @param lag A value for a lag, the max value is equal to the length of the shortest peptide minus one.
 #' @param property1 A property to use as value to evaluate the cross-covariance.
 #' @param property2 A property to use as value to evaluate the cross-covariance.
 #' @param center A logical value \code{TRUE} or \code{FALSE} if the property must be centered.
+#'
 #' @return The computed cross-covariance index for a given amino-acids sequence
 #' @references Cruciani, G., Baroni, M., Carosati, E., Clementi, M., Valigi, R., and Clementi, S. (2004) Peptide studies by means of principal properties of amino acids derived from MIF descriptors. J. Chemom. 18, 146-155.
 #' @examples
@@ -38,14 +40,19 @@ crossCovariance <-
            property2,
            center = TRUE) {
     if (center == TRUE) {
-      property1 <- scale(property1)[, ]
-      property2 <- scale(property2)[, ]
+      property1 <- scale(property1)[,]
+      property2 <- scale(property2)[,]
     }
     sequence <- gsub("[[:space:]]+", "", sequence)
-    sequence <- strsplit(sequence, "")
-    unlist(lapply(sequence, function(sequence) {
-      sum(sapply(seq_len(length(sequence) - lag), function(position) {
-        property1[sequence[[position]]] * property2[sequence[[position + lag]]]
-      }, USE.NAMES = FALSE)) / length(sequence)
-    }))
+    if (lag < (min(nchar(sequence)) - 1)) {
+      sequence <- strsplit(sequence, "")
+      unlist(lapply(sequence, function(sequence) {
+        sum(sapply(seq_len(length(sequence) - lag), function(position) {
+          property1[sequence[[position]]] * property2[sequence[[position + lag]]]
+        }, USE.NAMES = FALSE)) / length(sequence)
+      }))
+    } else {
+      stop("The lag max possible value must be equal to the length of shortest peptide minus one")
+    }
   }
+
