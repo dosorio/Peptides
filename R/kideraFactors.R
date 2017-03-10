314bed47d6793381b6dae73ce01ed002d1830f2d
Daniel Osorio <dcosorioh@unal.edu.co>
2017-03-10 17:47:11 -0500

    kideraFactors documentation was fixed

--- R/kideraFactors.R
+++ R/kideraFactors.R
@@ -20,8 +20,10 @@
 #' @examples 
 #' kideraFactors(seq = "KLKLLLLLKLK")
 #' # [[1]]
-#'      KF1         KF2         KF3         KF4         KF5         KF6         KF7         KF8         KF9        KF10 
-#' -0.78545455  0.29818182 -0.23636364 -0.08181818  0.21000000 -1.89363636  1.02909091 -0.51272727  0.11181818  0.81000000 
+#' #     KF1         KF2         KF3         KF4         KF5 
+#' # -0.78545455  0.29818182 -0.23636364 -0.08181818  0.21000000 
+#' #     KF6         KF7         KF8         KF9        KF10 
+#' # -1.89363636  1.02909091 -0.51272727  0.11181818  0.81000000 
 
 kideraFactors <- function (seq) {
   # Remove the break lines from the sequence
