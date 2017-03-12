4633d68aeb41b0bbe9483db715d7f438e46172e0
Daniel Osorio <dcosorioh@unal.edu.co>
2017-03-11 19:07:22 -0500

    blosumIndices documentation was fixed

--- R/blosumIndices.R
+++ R/blosumIndices.R
@@ -1,5 +1,4 @@
 #' @export blosumIndices
-#' @title blosumIndices
 #' @title Compute the BLOSUM62 derived indices of a protein sequence
 #' @description BLOSUM indices were derived of physicochemical properties that have been subjected to a VARIMAX analyses and an alignment matrix of the 20 natural AAs using the BLOSUM62 matrix.
 #' @references Georgiev, A. G. (2009). Interpretable numerical descriptors of amino acid space. Journal of Computational Biology, 16(5), 703-723.
