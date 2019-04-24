3fe0dbcef208343b99ce5b25197c2d4ec2b650d8
Aman Verma <aman.verma.mtl@gmail.com>
2019-04-24 17:31:00 +0000

    added test that warning is produced for invalid non-initial amino acid sequences

--- tests/testthat/test.aacheck.R
+++ tests/testthat/test.aacheck.R
@@ -1,5 +1,10 @@
 # NON STANDARD AMINO ACIDS PRESENT
+context("Check if amino acids are valid")
 
 test_that("Non standard aminoacids present in sequence",{expect_warning(
   aaCheck("KLRSTZMZ")
+)})
+
+test_that("Warning produced when non-standard amino acids present in any sequence except the first",{expect_warning(
+  aaCheck(c("KLR", "KLRSTZMZ"))
 )})
