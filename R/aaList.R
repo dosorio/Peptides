113721c2678c315f775e943a37b5e6244e31c1b4
Daniel Osorio <dcosorioh@tamu.edu>
2020-05-10 13:54:17 -0500

    Manual updated

--- R/aaList.R
+++ R/aaList.R
@@ -2,7 +2,7 @@
 #' @title Return a vector with the 20 standard amino acids in upper case
 #' @description This function returns a vector with the 20 standard amino acids in upper case. 
 
-#' @return A character vector with the 20 standard amino acids in upper case
+#' @return A character vector with the 20 standard amino acids in upper case.
 #' @references Lu, Y., & Freeland, S. (2006). On the evolution of the standard amino-acid alphabet. Genome biology, 7(1), 102.
 #' @author  Richel Bilderbeek <richel@richelbilderbeek.nl>
 #' 
