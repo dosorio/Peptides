140e48f280dee0b45fa9ccf29cb21bde9e9e4fea
Florian Christoph Sigloch <florian.sigloch@mailbox.org>
2020-10-20 16:09:26 +0200

    Completed massShift function, adapted mw function and added mz function

--- R/massShift.R
+++ R/massShift.R
@@ -2,9 +2,9 @@
 #' @title Calculate the mass difference of modified peptides.
 #' @description This function calculates the mass difference of peptides introduced by chemical modifications or heavy isotope labelling.
 #' @param seq An amino-acids sequence, in one letter code.
-#' @param label Set a predefined heavy isotope label. Accepts "none", "silac_13c", "silac_13c15n" and "15n". Overwrites input in \code{aa} and \code{shift}.
-#' @param aa Define modified amino acids. Multiple amino acids carrying the same modification can be given as a single string. Multiple amino acids carrying different modifications can be given as a vector.
-#' @param shift Define the mass difference in Dalton of the amino acid(s) given in \code{aa}. If \code{aa} is a vector, \code{shift} must also be a vector of the same length.
+#' @param monoisotopic A logical value \code{'TRUE'} or \code{'FALSE'} indicating if monoisotopic weights of amino-acids should be used
+#' @param label Set a predefined heavy isotope label. Accepts "none", "silac_13c", "silac_13c15n" and "15n". Overwrites input in \code{shift}.
+#' @param aaShift Define the mass difference in Dalton of given amino acids as a named vector. Use the amino acid one letter code as names and the mass shift in Dalton as values. 
 #' @source  For the predefined heavy isotope labels, compare:
 #' \itemize{
 #'  \item{silac_13c}{
@@ -20,100 +20,67 @@
 #'    \href{http://www.unimod.org/modifications_view.php?editid1=996}{Unimod 996} and 
 #'    \href{http://www.unimod.org/modifications_view.php?editid1=897}{Unimod 897}
 #'  }
+#'  \item{c57}{
+#'    \href{http://www.unimod.org/modifications_view.php?editid1=4}{Unimod 4}
+#'  }
+#' }
 #' @details 
 #' @examples 
-#' massShift("EGVNDNEEGFFSAR", label = "silac_13c")
-#' massShift("EGVNDNEEGFFSAR", aa = "R", shift = 6.020129)
+#' massShift("EGVNDNECEGFFSAR", label = "silac_13c")
+#' massShift("EGVNDNECEGFFSAR", aaShift = c(K = 6.020129, R = 6.020129))
+#' massShift("EGVNDNECEGFFSAR", label = "silac_13c", c57=FALSE)
 #'
-#'# To define multiple amino acids with the same modification, set \code{aa} as a single string.
-#' massShift("EGVNDNEEGFFSARK", aa = "RK", shift = 6.020129)
-#'
-#' # To define multiple amino acids with different modifications, set \code{aa} and \code{shift} as character vectors.
-#' massShift("EGVNDNEEGFFSAR", aa = c("RK", "C"), shift = c(6.020129, 57.021464))
 
 massShift <-
-function(seq, label = "none", aa, shift){
+function(seq, label = "none", aaShift = NULL, monoisotopic = TRUE){
 
   # Check inputs
   label <- tolower(label) # Renders case-insensitive input string.
   if(!(label %in% c("none", "silac_13c", "silac_13c15n", "15n"))){
     stop("Given label type unknown. Please use one of 'none', '15N', 'Silac_13C15N', or 'Silac_13C' (case-insensitive).")
   }
-  if(label == "none" & ((!missing("aa") & missing("shift")) | (missing("aa") & !missing("shift")))){
-    stop("Both 'aa' and 'shift' must be defined.")
-  }
-  if(!missing("aa") & !missing("shift")){
-    if(length(shift) != 1 & length(shift) != length(aa)){
-      stop("If 'aa' is given, 'shift' must be a vector of length 1 or of the same length as 'aa'.")
-    }
+  if(!is.null(aaShift) & is.null(names(aaShift))){
+    stop("'aaShift' must be given as a named vector, e.g. 'aaShift = c(K = 6.020129)'.")
   }
   
   # Predefined labels
   if (label == "silac_13c"){
-    aa <- "KR"
-    shift <- 6.020129
+    aaShift <- c("K" = 6.020129 - 0.064229*!monoisotopic, "R" = 6.020129- 0.064229*!monoisotopic)
   } else if(label == "silac_13c15n"){
-    aa <- c("K", "R")
-    shift <- c(8.014199, 10.008269)
+    aaShift <- c("K" = 8.014199 -0.071499*!monoisotopic, "R" = 10.008269-0.078669*!monoisotopic)
+  } else if(label == "15n"){
+    aaShift <- c(
+      A = 1,
+      R = 4,
+      N = 2,
+      D = 1,
+      C = 1,
+      E = 1,
+      Q = 2,
+      G = 1,
+      H = 3,
+      I = 1,
+      L = 1,
+      K = 2,
+      M = 1,
+      F = 1,
+      P = 1,
+      S = 1,
+      T = 1,
+      W = 2,
+      Y = 1,
+      V = 1,
+      U = 1,
+      O = 3
+    ) * 0.997035 -0.003635*!monoisotopic # 0.997035 equals the mass shift from 14N to 15N. 0.9934 equals the average mass shift.
   }
   
   # Split sequence by amino acids
   seq <- aaCheck(seq)
 
-  # Add mass shifts
-  if(label != "15n"){
-    shiftTotal <- 0
-    if(!missing(aa)){
-      aa <- as.list(aa)
-      aa <- lapply(aa, function(x) unlist(strsplit(x, "")))
-      for (i in 1:length(aa)){
-        for (j in 1:length(aa[[i]])){
-          n <- stri_count(seq, fixed = aa[[i]][j])
-          if (length(shift) < length(aa)) {
-            shiftTotal <- shiftTotal + n * shift
-          } else {
-            shiftTotal <- shiftTotal + n * shift[i]
-          }
-        }
-      }
-    }
-  }else if(label == "15n"){
-    # Add mass shifts for 15N labelling
-    # Create vector with the number of nitrogen atoms per amino acid.
-    countN <-
-      c(
-        A = 1,
-        R = 4,
-        N = 2,
-        D = 1,
-        C = 1,
-        E = 1,
-        Q = 2,
-        G = 1,
-        H = 3,
-        I = 1,
-        L = 1,
-        K = 2,
-        M = 1,
-        F = 1,
-        P = 1,
-        S = 1,
-        T = 1,
-        W = 2,
-        Y = 1,
-        V = 1,
-        U = 1,
-        O = 3
-      )
-  #   shiftTotal <- 0
-  #   for (i in 1:length(countN)){
-  #     nN <- sum(stri_count(unlist(seq), fixed = names(countN[i]))) * as.numeric(countN[i])
-  #     shiftTotal <- shiftTotal + nN * 0.997035 # 0.997035 equals the mass shift from 14N to 15N.
-  #   }
-    shiftTotal <- unlist(lapply(seq, function(x){
-        sum(countN[c(x)])
-      })) * 0.997035 # 0.997035 equals the mass shift from 14N to 15N.
-  }
-
-  return(shiftTotal)
+  # Calculate mass shifts
+  unlist(
+    lapply(seq, function(x){
+      sum(aaShift[c(x)], na.rm = TRUE)
+      }))
 }
---
+++ R/mz.R
@@ -0,0 +1,34 @@
+#' @export mz
+#' @title Calculate the m/z for peptides.
+#' @description This function calculates the (monoisotopic) mass over charge ratio (m/z) for peptides, as measured in mass spectrometry.
+#' @param seq An amino-acids sequence, in one letter code.
+#' @param label Set a predefined heavy isotope label. Accepts "none", "silac_13c", "silac_13c15n" and "15n". Overwrites input in \code{shift}.
+#' @param aaShift Define the mass difference in Dalton of given amino acids as a named vector. Use the amino acid one letter code as names and the mass shift in Dalton as values. 
+#' @param cysteins Define the mass shift in Dalton of blocked cysteins. Defaults to 57.021464, for cysteins blocked by iodoacetamide.
+#' @examples 
+#' mz("EGVNDNECEGFFSAR")
+#' mz("EGVNDNECEGFFSAR", aaShift = c(K = 6.020129, R = 6.020129))
+#' mz("EGVNDNECEGFFSAR", label = "silac_13c", cysteins = 58.005479)
+#'
+mz <-
+  function(seq, charge=2, label="none", aaShift = NULL, cysteins = 57.021464){
+    
+    # Check for correct input
+    if(!is.numeric(charge) | length(charge) != 1){
+      stop("Charge must be given as an integer (typically between 1-4).")
+    }
+    
+    # Calculate mass of uncharged peptide
+    mass <- mw(seq=seq, label=label, aaShift=aaShift, monoisotopic = TRUE)
+    
+    # Add modification at cysteins
+    mass <- mass + stri_count(x, fixed = "C") * cysteins
+    
+    # Modify for charged peptides.
+    if (charge >= 0){
+      mass <- mass + charge * 1.007276 # Add weight of H+ ions.
+      mass <- mass / charge # Divide by charge state.
+    }
+    
+    return(mass)
+  }
